24b417ca0939fcd2bc86788e79c0129ba3736011
Reazul Islam <reazulislam137@gmail.com>
2018-04-06 20:27:48 +0600

    first commit

---
+++ april-06-01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int j, sum = 0;
+
+    printf("The first 10 natural number is : \n");
+
+    for (j=1; j<=10; j++)
+    {
+        sum = sum+j;
+        printf(" %d",j);
+    }
+
+    printf("\n The sum is %d \n",sum);
+
+    return 0;
+}
